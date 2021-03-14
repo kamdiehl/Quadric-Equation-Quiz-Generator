6b25112ce3c09a8d5fa603a67343cc60d29a0173
kamdiehl <kamryndiehl57@gmail.com>
2021-03-14 12:05:05 -0700

    Phase 3 submission #2: Image added to title page and methods are sorted into private/public.

--- src/main/ui/QuadricGui.java
+++ src/main/ui/QuadricGui.java
@@ -1,66 +1,112 @@
 package ui;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.plaf.ColorUIResource;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 public class QuadricGui extends JFrame implements ActionListener {
     private JLabel label;
-    private JTextField field;
-    private ImageIcon titleIcon;
+    private ImageIcon unscaledTitleIcon;
     private JLabel titlePageLabel;
-    private JFrame frame;
+    private JTextField field;
 
     public static final int WIDTH = 1000;
     public static final int HEIGHT = 700;
-    public static final int TITLE_FONT = 24;
+    public static final int IMAGE_WIDTH = 450;
+    public static final int IMAGE_HEIGHT = 470;
+    public static final int TITLE_FONT = 27;
     public static final String TITLE_TEXT = "QUADRIC EQUATION QUIZ";
-    public static final Color TITLE_COLOR = new Color(1, 108, 83);
+
+    public static final Color TITLE_COLOR = new Color(1, 108, 104);
+    public static final Color startBtnColor = new Color(0, 198, 183);
+    public static final Color instructionBtnColor = new Color(5, 170, 157);
+    public static final Color loadBtnColor = new Color(4, 118, 109);
+    public static final Color exitBtnColor = new Color(2, 92, 94);
 
 
 
     // Constructor
-    // EFFECTS:
+    // EFFECTS: Initializes everything needed for quiz
     public QuadricGui() {
         super("Quadric App");
-        initializeGraphics();
 
-        //titleIcon = new ImageIcon(this.getClass().getResource("/hyp1sh.jpg"));
-       // titlePageLabel = new JLabel(titleIcon);
-        //titlePageLabel.setSize(500, 200);
-
-        //setLayout(new FlowLayout());
         setLayout(new GridBagLayout());
         setLayout(new BorderLayout());
 
-        // title
-        JLabel titleLabel = new JLabel(TITLE_TEXT, SwingConstants.CENTER);
-        titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD, TITLE_FONT));
-        JPanel titlePanel = new JPanel();
-        titlePanel.add(titleLabel);
         JPanel topPanel = new JPanel();
-        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.PAGE_AXIS));
-        topPanel.add(titlePanel);
-        add(topPanel, BorderLayout.NORTH);
-
-        //topPanel.add(titlePageLabel);
-
-        // start & instruction buttons
         JPanel bottomPanel = new JPanel(new GridLayout(4, 4, 1, 1));
+
+        initializeGraphics();
+        initializeTitlePage(topPanel, bottomPanel);
+
+        // startBtn.setActionCommand("myButton");
+        // startBtn.addActionListener(this);
+
+        // from project guide
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        ((JPanel) getContentPane()).setBorder(new EmptyBorder(13, 13, 13, 13));
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+
+    }
+
+
+    // MODIFIES: this
+    // EFFECTS:  draws the JFrame window where this quadric app will operate
+    public void initializeGraphics() {
+        setLayout(new BorderLayout());
+        setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        // background colours
+        setBackground(Color.cyan);
+        setForeground(Color.blue);
+    }
+
+
+    // MODIFIES: this
+    // EFFECTS:  initializes the title page that starts the quiz
+    public void initializeTitlePage(JPanel topPanel, JPanel bottomPanel) {
+        initializeTitleButtons(bottomPanel);
+        initializeTitle(topPanel);
+        initializeTitleImage(topPanel);
+    }
+
+
+
+    // EFFECTS: Method that is called when the the JButton btn is clicked
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("myButton")) {
+            label.setText(field.getText());
+        }
+
+    }
+
+
+
+
+
+
+
+    // PRIVATE METHODS ---------------------------------------------------------------------------------------
+
+
+    // Title Page and Menu Option Methods:
+
+    // EFFECTS: Initializes the menu options on the title page
+    private void initializeTitleButtons(JPanel bottomPanel) {
 
         JButton startBtn = new JButton("START");
         JButton instructionBtn = new JButton("HOW TO PLAY");
         JButton loadBtn = new JButton("LOAD DATA");
         JButton exitBtn = new JButton("EXIT");
-
-        startBtn.setActionCommand("myButton");
-        startBtn.addActionListener(this);
 
         bottomPanel.add(startBtn);
         bottomPanel.add(instructionBtn);
@@ -68,36 +114,7 @@ public class QuadricGui extends JFrame implements ActionListener {
         bottomPanel.add(exitBtn);
         add(bottomPanel, BorderLayout.SOUTH);
 
-
-
-
-        // from project guide
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        ((JPanel) getContentPane()).setBorder(new EmptyBorder(13, 13, 13, 13));
-
-
-
-        //label = new JLabel("flag");
-        //field = new JTextField(5);
-       // add(field);
-
-        //add(label);
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
-        setResizable(false);
-
-
-        // background colours
-        setBackground(Color.cyan);
-        setForeground(Color.blue);
-
-        Color startBtnColor = new Color(0, 198, 183);
-        Color instructionBtnColor = new Color(5, 170, 157);
-        Color loadBtnColor = new Color(4, 118, 109);
-        Color exitBtnColor = new Color(2, 92, 94);
-
+        // background button colours
         startBtn.setBackground(startBtnColor);
         startBtn.setForeground(exitBtnColor);
         startBtn.setOpaque(true);
@@ -113,28 +130,42 @@ public class QuadricGui extends JFrame implements ActionListener {
     }
 
 
-
-
-    // MODIFIES: this
-    // EFFECTS:  draws the JFrame window where this quadric app will operate, and populates the tools to be used
-    //           to manipulate this drawing
-    public void initializeGraphics() {
-        setLayout(new BorderLayout());
-        setMinimumSize(new Dimension(WIDTH, HEIGHT));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
+    // EFFECTS: Initializes the title text that displays title name on title page.
+    private void initializeTitle(JPanel topPanel) {
+        JLabel titleLabel = new JLabel(TITLE_TEXT, SwingConstants.CENTER);
+        titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD, TITLE_FONT));
+        titleLabel.setForeground(TITLE_COLOR);
+        JPanel titlePanel = new JPanel();
+        titlePanel.add(titleLabel);
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.PAGE_AXIS));
+        topPanel.add(titlePanel);
+        add(topPanel, BorderLayout.NORTH);
     }
 
 
+    // MODIFIES: This
+    // EFFECTS: Initializes the image that appears on the title page.
+    private void initializeTitleImage(JPanel topPanel) {
+        unscaledTitleIcon = createImageIcon("/images/hyp1sh.jpg", "title picture"); // create the icon
+        Image titleImage = unscaledTitleIcon.getImage(); // convert it into an image
+        Image finalTitleImage = titleImage.getScaledInstance(IMAGE_WIDTH, IMAGE_HEIGHT,  java.awt.Image.SCALE_SMOOTH);
+        unscaledTitleIcon = new ImageIcon(finalTitleImage); // convert it back to an image icon
+        label = new JLabel("Image and Text", unscaledTitleIcon, JLabel.CENTER);
+        titlePageLabel = new JLabel(unscaledTitleIcon);
+        topPanel.add(titlePageLabel);
+    }
 
 
-
-    // EFFECTS: Method that is called when the the JButton btn is clicked
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("myButton")) {
-            label.setText(field.getText());
+    // EFFECTS: Creates the title image icon
+    protected ImageIcon createImageIcon(String path, String description) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
         }
-
     }
+
+
 }
