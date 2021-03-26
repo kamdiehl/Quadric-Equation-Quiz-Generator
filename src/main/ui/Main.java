9f516840e83505f0af2fe61f3720e2c49153cee4
kamdiehl <kamryndiehl57@gmail.com>
2021-03-26 16:53:40 -0700

    Phase 3 submission #29: New error, can't run another quiz after saving previous one. Going to refactor getAllStats since it might have something to do with the "unmodifiable list".

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,15 +3,25 @@ package ui;
 import ui.graphics.MainFrame;
 
 
+import javax.swing.*;
 import java.awt.geom.QuadCurve2D;
 import java.io.FileNotFoundException;
 
 public class Main {
     // This class is the main class that runs the app.
     public static void main(String[] args) {
-        new MainFrame();
+        //new MainFrame();
 
-        //new QuadricGui();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame();
+
+            }
+        });
+
+
+                //new QuadricGui();
 //        try {
 //            new QuadricApp();
 //
